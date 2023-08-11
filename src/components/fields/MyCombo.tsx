12cab37250ec605e90144334539c4b9c7cb33528
lsimo <leslie.simo@student.uclouvain.be>
2023-08-11 08:48:34 +0200

    cleaning

--- src/components/fields/MyCombo.tsx
+++ src/components/fields/MyCombo.tsx
@@ -4,7 +4,7 @@ import findCountries, { Country } from "../../country-service"
 import { useQuery } from '@tanstack/react-query'
 
 export default function MyCombobox() {
-  const [selectedPerson, setSelectedPerson] = useState("Saudi Arabia")
+  const [selectedPerson, setSelectedPerson] = useState("")
   const [query, setQuery] = useState('')
 
   const { data, isLoading, isError } = useQuery<Country[], Error>(
@@ -12,23 +12,20 @@ export default function MyCombobox() {
     () => findCountries(query),
 
   );
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (isError) {
-    return <div>Error fetching data</div>;
-  }
 
-  if (!data) {
-    return <div>No data available</div>;
-  }
-  console.log("first: ", isLoading)
  
   return (
-    <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+    <div>
+      <div>Select Country:</div>
+      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
       <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
       <Combobox.Options>
-        { data && (data as Country[]).map((country:Country) => (
+        { isLoading? <div>Loading...</div>:
+        isError? <div>Error fetching data</div>:
+        !data? <div>No data available</div>:
+        
+        
+        data && (data as Country[]).map((country:Country) => (
           <Combobox.Option key={country.code} value={country.name}>
             {country.name.common}
           </Combobox.Option>
@@ -36,5 +33,8 @@ export default function MyCombobox() {
         }
       </Combobox.Options>
     </Combobox>
+
+    </div>
+
   )
 }
